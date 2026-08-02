88e0a86e6607d2003dc7dd9598837c025e5f75b2
Hugh Saalmans <hugh.saalmans@gmail.com>
2026-08-02 18:02:07 +1000

    pb_tool not working

--- scripts/01_setup_python_env.sh
+++ scripts/01_setup_python_env.sh
@@ -2,16 +2,18 @@
 
 # --------------------------------------------------------------------------------------------------------------------
 
+REPO_FOLDER="/Users/$(whoami)/git/minus34/bigquerylayers"
+
 PYTHON_EXECUTABLE="/Applications/QGIS-final-4_2_0.app/Contents/MacOS/python"
-PYTHONPATH="/Applications/QGIS-final-4_2_0.app/Contents/Resources/python3.12"
-# PYTHON_VERSION="3.12"
 
 # --------------------------------------------------------------------------------------------------------------------
 
 conda deactivate
 
-export PYTHONHOME=/Applications/QGIS-final-4_2_0.app/Contents/MacOS
-export PYTHONPATH=/Applications/QGIS-final-4_2_0.app/Contents/Resources/python3.12
+cd $REPO_FOLDER
+
+export PYTHONHOME="/Applications/QGIS-final-4_2_0.app/Contents/MacOS"
+export PYTHONPATH="/Applications/QGIS-final-4_2_0.app/Contents/Resources/python3.12:/Applications/QGIS-final-4_2_0.app/Contents/Resources/python3.12/site-packages"
 
 # install pip
 $PYTHON_EXECUTABLE -m ensurepip --default-pip
@@ -20,7 +22,10 @@ $PYTHON_EXECUTABLE -m ensurepip --default-pip
 $PYTHON_EXECUTABLE -m pip install pip setuptools wheel
 
 # install this plugin's prerequisites
-$PYTHON_EXECUTABLE -m pip install -r /Users/$(whoami)/git/minus34/bigquerylayers/requirements.txt
+$PYTHON_EXECUTABLE -m pip install -r requirements.txt
 
 #install plugin developer packages
 $PYTHON_EXECUTABLE -m pip install PyQt6 qt6-tools PySide6 pb_tool ptvsd debugpy pytest
+
+
+$PYTHON_EXECUTABLE -m pip install binascii
--- scripts/02_compile_plugin.sh
+++ scripts/02_compile_plugin.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
-# restart environment
-conda activate qgis4_dev
+# --------------------------------------------------------------------------------------------------------------------
+
+REPO_FOLDER="/Users/$(whoami)/git/minus34/bigquerylayers"
+
+PYTHON_EXECUTABLE="/Applications/QGIS-final-4_2_0.app/Contents/MacOS/python"
+
+# --------------------------------------------------------------------------------------------------------------------
+
+conda deactivate
+
+cd $REPO_FOLDER
+
+export PYTHONHOME="/Applications/QGIS-final-4_2_0.app/Contents/MacOS"
+export PYTHONPATH="/Applications/QGIS-final-4_2_0.app/Contents/Resources/python3.12:/Applications/QGIS-final-4_2_0.app/Contents/Resources/python3.12/site-packages"
+export PATH="/Applications/QGIS-final-4_2_0.app/Contents/Frameworks/bin:$PATH"
 
 # add path to rcc compiler
-export PATH="/Users/hughsaalmans/miniconda3/envs/qgis4_dev/lib/python3.12/site-packages/PySide6/Qt/libexec:$PATH"
+# export PATH="/Users/$(whoami)/miniconda3/envs/qgis4_dev/lib/python3.12/site-packages/PySide6/Qt/libexec:$PATH"
 
 # compile plugin
-cd /Users/hughsaalmans/git/minus34/bigquerylayers
-pb_tool compile
+/Applications/QGIS-final-4_2_0.app/Contents/Frameworks/bin/pb_tool compile
